1329c87253772ae788202bceb5e80c86e5b50136
Vitali <vitali_t90@mail.ru>
2012-02-02 21:00:48 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFinish::Application.config.secret_token = 'caa8f26da6232d2b290c3825003f2eacb72e3e16d00c701325d5b993f4580880d90bd5604ebad012b7bb0e15f75340d25846b4ab37bcadb14cc7a8b65eea3744'
