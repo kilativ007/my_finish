dc80756f3c33835f43181a94897f391afdbf9916
Vitali <vitali_t90@mail.ru>
2012-02-05 03:50:43 +0200

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Kursusetoo application pages"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
+
+ 
+ 
+
